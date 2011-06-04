01e13d92b426274c54c35f8dc93f1f4afb77042f
Pavel Tiunov <pavel.tiunov@gmail.com>
2011-06-05 00:03:09 +0400

    Sideral time conversion j2000 zero case

--- sade-lab/src/main/scala/org/sade/starcoords/StarCoordsConverter.scala
+++ sade-lab/src/main/scala/org/sade/starcoords/StarCoordsConverter.scala
@@ -1,6 +1,7 @@
 package org.sade.starcoords
 
 import scala.math._
+import java.util.{TimeZone, GregorianCalendar, Date, Calendar}
 
 object StarCoordsConverter {
   def radiansFrom(grad: Double) = {
@@ -11,9 +12,22 @@ object StarCoordsConverter {
     radians * 180 / Pi
   }
 
-  def toDeltaTime(coords: LabCoordinates) = {
-    val thetaRad = radiansFrom(coords.theta)
-    val phiRad = radiansFrom(coords.phi)
+  def toHourAngle(radians: Double) = radians * 12 / Pi
+
+  def fromHourAngle(angle: Double) = angle * Pi / 12
+
+
+  def sideralTime(coords: FullStandCoordinate) = {
+    val time = coords.stand.time
+    val j2000 = new GregorianCalendar(TimeZone.getTimeZone("UTC"))
+    j2000.set(2000, 0, 1, 12, 0)
+    val d = new Date(time.getTime - j2000.getTime.getTime).getTime / 1000.0 / 3600.0 / 24.0
+    fromHourAngle(18.697374558 + 24.06570982441908 * d)
+  }
+
+  def toDeltaTime(coords: FullStandCoordinate) = {
+    val thetaRad = radiansFrom(coords.stand.theta)
+    val phiRad = radiansFrom(coords.labCoord.latitude)
     val deltaRad = asin(cos(thetaRad) * cos(phiRad))
     val cosDelta = cos(deltaRad)
     val tRad = atan2(-sin(thetaRad) / cosDelta, -sin(phiRad) * cos(thetaRad) / cosDelta)
@@ -21,5 +35,10 @@ object StarCoordsConverter {
   }
 }
 
-case class LabCoordinates(theta: Double, phi: Double)
+case class FullStandCoordinate(stand: StandCoordinate, labCoord: LabCoordinates)
+
+case class StandCoordinate(theta: Double, time: Date)
+
+case class LabCoordinates(longitude: Double, latitude: Double)
+
 case class DeltaTime(delta: Double, t: Double)
--- sade-lab/src/test/scala/org/sade/analyzers/starcoords/StarCoordsConverterTest.scala
+++ sade-lab/src/test/scala/org/sade/analyzers/starcoords/StarCoordsConverterTest.scala
@@ -3,26 +3,38 @@ package org.sade.analyzers.starcoords
 import org.scalatest.Spec
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.MustMatchers
-import org.sade.starcoords.{DeltaTime, LabCoordinates, StarCoordsConverter}
+import org.sade.starcoords._
+import org.scalatest.matchers.{MustMatchers}
+import java.util.{TimeZone, GregorianCalendar}
 
 @RunWith(classOf[JUnitRunner])
 class StarCoordsConverterTest extends Spec with MustMatchers {
-  def assertCoords(coords: LabCoordinates, expectedDelta: Double, expectedT: Double): Unit = {
+  def assertCoords(coords: FullStandCoordinate, expectedDelta: Double, expectedT: Double) {
     val deltaTime = StarCoordsConverter.toDeltaTime(coords)
     deltaTime.delta must be(expectedDelta.plusOrMinus(1E-5))
     deltaTime.t must be(expectedT.plusOrMinus(1E-5))
   }
 
   describe("The Converter") {
-    it ("should convert polar case") {
-      assertCoords(LabCoordinates(15, 90), 0d, -165d)
+    it("should convert polar case") {
+      assertCoords(FullStandCoordinate(StandCoordinate(15, null), LabCoordinates(15, 90)), 0d, -165d)
     }
 
-    it ("should convert equator case") {
-      assertCoords(LabCoordinates(0, 0), 90, -180)
-      assertCoords(LabCoordinates(90, 0), 0, -90)
-      assertCoords(LabCoordinates(-90, 0), 0, 90)
+    it("should convert equator case") {
+      assertCoords(FullStandCoordinate(StandCoordinate(0, null), LabCoordinates(0, 0)), 90, -180)
+      assertCoords(FullStandCoordinate(StandCoordinate(90, null), LabCoordinates(90, 0)), 0, -90)
+      assertCoords(FullStandCoordinate(StandCoordinate(-90, null), LabCoordinates(-90, 0)), 0, 90)
+    }
+  }
+
+  describe("Sideral time converter") {
+    it("should convert from zero J2000") {
+      val calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"))
+      calendar.set(2000, 0, 1, 12, 0)
+      StarCoordsConverter.sideralTime(FullStandCoordinate(
+        StandCoordinate(0, calendar.getTime),
+        LabCoordinates(0, 0))
+      ) must be(4.89.plusOrMinus(1E-2))
     }
   }
 }
