65a5f3801a022b23dc287572938ad06ed403cd90
Pavel Tiunov <pavel.tiunov@gmail.com>
2011-06-05 11:50:51 +0400

    Galactic coordinate converter works

--- sade-lab/src/main/scala/org/sade/starcoords/StarCoordsConverter.scala
+++ sade-lab/src/main/scala/org/sade/starcoords/StarCoordsConverter.scala
@@ -2,8 +2,15 @@ package org.sade.starcoords
 
 import scala.math._
 import java.util.{TimeZone, GregorianCalendar, Date, Calendar}
+import org.apache.commons.math.linear.MatrixUtils
 
 object StarCoordsConverter {
+  val galacticMatrix = MatrixUtils.createRealMatrix(Array(
+    Array(-0.05487556, -0.87343709, -0.48383501),
+    Array(0.49410942, -0.44482969, 0.74698224),
+    Array(-0.86766614, -0.19807637, 0.45598377)
+  ))
+
   def radiansFrom(grad: Double) = {
     grad * Pi / 180
   }
@@ -22,16 +29,39 @@ object StarCoordsConverter {
     val j2000 = new GregorianCalendar(TimeZone.getTimeZone("UTC"))
     j2000.set(2000, 0, 1, 12, 0)
     val d = new Date(time.getTime - j2000.getTime.getTime).getTime / 1000.0 / 3600.0 / 24.0
-    fromHourAngle(18.697374558 + 24.06570982441908 * d)
+    gradFrom(fromHourAngle(18.697374558 + 24.06570982441908 * d)) + coords.labCoord.longitude
+  }
+
+  def toGalacticCoordinates(equatorial: EquatorialCoordinates) = {
+    val deltaRad = radiansFrom(equatorial.delta)
+    val alphaRad = radiansFrom(equatorial.alpha)
+    val rhsMatrix = MatrixUtils.createColumnRealMatrix(Array(cos(deltaRad) * cos(alphaRad), cos(deltaRad) * sin(alphaRad), sin(deltaRad)))
+    val product = galacticMatrix.multiply(rhsMatrix)
+    val solution = solveSphericalTriangleEquation(product.getEntry(0, 0), product.getEntry(1, 0), product.getEntry(2, 0))
+    GalacticCoordinates(gradFrom(solution._1), gradFrom(solution._2))
+  }
+
+  def toEquatorial(coords: FullStandCoordinate) = {
+    val deltaTime = toDeltaTime(coords)
+    EquatorialCoordinates(deltaTime.delta, sideralTime(coords) - deltaTime.t)
+  }
+
+  def solveSphericalTriangleEquation(firstEquationArg: Double, secondEquationArg: Double, thirdEquationArg: Double) = {
+    val deltaRad = asin(thirdEquationArg)
+    val cosDelta = cos(deltaRad)
+    val tRad = atan2(secondEquationArg / cosDelta, firstEquationArg / cosDelta)
+    (deltaRad, tRad)
   }
 
   def toDeltaTime(coords: FullStandCoordinate) = {
     val thetaRad = radiansFrom(coords.stand.theta)
     val phiRad = radiansFrom(coords.labCoord.latitude)
-    val deltaRad = asin(cos(thetaRad) * cos(phiRad))
-    val cosDelta = cos(deltaRad)
-    val tRad = atan2(-sin(thetaRad) / cosDelta, -sin(phiRad) * cos(thetaRad) / cosDelta)
-    DeltaTime(gradFrom(deltaRad), gradFrom(tRad))
+    val result = solveSphericalTriangleEquation(
+      -sin(phiRad) * cos(thetaRad),
+      -sin(thetaRad),
+      cos(thetaRad) * cos(phiRad)
+    )
+    DeltaTime(gradFrom(result._1), gradFrom(result._2))
   }
 }
 
@@ -42,3 +72,7 @@ case class StandCoordinate(theta: Double, time: Date)
 case class LabCoordinates(longitude: Double, latitude: Double)
 
 case class DeltaTime(delta: Double, t: Double)
+
+case class EquatorialCoordinates(delta: Double, alpha: Double)
+
+case class GalacticCoordinates(b: Double, l: Double)
--- sade-lab/src/test/scala/org/sade/analyzers/starcoords/StarCoordsConverterTest.scala
+++ sade-lab/src/test/scala/org/sade/analyzers/starcoords/StarCoordsConverterTest.scala
@@ -6,6 +6,7 @@ import org.scalatest.junit.JUnitRunner
 import org.sade.starcoords._
 import org.scalatest.matchers.{MustMatchers}
 import java.util.{TimeZone, GregorianCalendar}
+import math._
 
 @RunWith(classOf[JUnitRunner])
 class StarCoordsConverterTest extends Spec with MustMatchers {
@@ -28,13 +29,37 @@ class StarCoordsConverterTest extends Spec with MustMatchers {
   }
 
   describe("Sideral time converter") {
+    val j2000 = new GregorianCalendar(TimeZone.getTimeZone("UTC"))
+    j2000.set(2000, 0, 1, 12, 0)
+
     it("should convert from zero J2000") {
-      val calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"))
-      calendar.set(2000, 0, 1, 12, 0)
       StarCoordsConverter.sideralTime(FullStandCoordinate(
-        StandCoordinate(0, calendar.getTime),
+        StandCoordinate(0, j2000.getTime),
         LabCoordinates(0, 0))
-      ) must be(4.89.plusOrMinus(1E-2))
+      ) must be(280.46.plusOrMinus(1E-2))
+    }
+
+    it("should convert from zero J2000 + longitude") {
+      StarCoordsConverter.sideralTime(FullStandCoordinate(
+        StandCoordinate(0, j2000.getTime),
+        LabCoordinates(90, 0))
+      ) must be((370.46).plusOrMinus(1E-2))
+    }
+  }
+
+  def assertGalacticCoords(coords: EquatorialCoordinates, expectedB: Double, expectedL: Double) {
+    val result = StarCoordsConverter.toGalacticCoordinates(coords)
+    result.b must be(expectedB.plusOrMinus(1E-2))
+    result.l must be(expectedL.plusOrMinus(1E-2))
+  }
+
+  describe("Galactic converter") {
+    it("should convert zero case") {
+      assertGalacticCoords(EquatorialCoordinates(0, 0), -60.1887, 96.3379)
+    }
+
+    it("should convert some arbitrary case") {
+      assertGalacticCoords(EquatorialCoordinates(90, 90), 27.1282, 122.9319)
     }
   }
 }
