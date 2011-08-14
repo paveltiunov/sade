a1baacaf948f4445a7565769d8fd573143b53f24
Pavel Tiunov <pavel.tiunov@gmail.com>
2011-08-14 18:57:57 +0400

    Extract star coords painter

--- sade-lab/src/main/scala/org/sade/starcoords/StarCoordsPlotter.scala
+++ sade-lab/src/main/scala/org/sade/starcoords/StarCoordsPlotter.scala
@@ -19,17 +19,25 @@ class StarCoordsPlotter extends Component {
     repaint()
   }
 
+  override protected def paintComponent(g: Graphics2D) {
+    StarCoordsPainter(filteredPoints, viewMode, size.getWidth.toInt, size.getHeight.toInt).paint(g)
+  }
+}
+
+case class StarCoordsPainter(points: Seq[SkyMapPoint], viewMode: ViewMode, width: Int, height: Int) {
+  val cmbrPoints: Seq[GalacticCoordinates] = Seq(GalacticCoordinates(48, 264))
+
   def drawColorPoint(g: Graphics2D)(p: NormalizedColorPoint) {
     val hue = p.color.toFloat * 5.0f/6.0f
     val color = new Color(Color.HSBtoRGB(hue, 1.0f, 1.0f))
     g.setColor(new Color(color.getRed, color.getGreen, color.getBlue, 200))
-    g.fillOval((p.point.x * size.getWidth).toInt - 2, (p.point.y * size.getHeight).toInt - 2, 4, 4)
+    g.fillOval((p.point.x * width).toInt - 2, (p.point.y * height).toInt - 2, 4, 4)
   }
 
   def drawCMBRPoint(g: Graphics2D)(p: NormalizedCMBR) {
     g.setColor(Color.black)
-    val centerX = (p.point.x * size.getWidth).toInt
-    val centerY = (p.point.y * size.getHeight).toInt
+    val centerX = (p.point.x * width).toInt
+    val centerY = (p.point.y * height).toInt
     g.drawRect(centerX - 5, centerY - 5, 10, 10)
     g.drawLine(centerX - 5, centerY - 5, centerX + 5, centerY + 5)
     g.drawLine(centerX + 5, centerY - 5, centerX - 5, centerY + 5)
@@ -46,12 +54,12 @@ class StarCoordsPlotter extends Component {
   }
 
   private def planePlot(g: Graphics2D) {
-    val normalized = normalize(filteredPoints.map(_.rotationAngle)) zip normalize(filteredPoints.map(_.time.getTime.toDouble)) zip normalize(filteredPoints.map(_.value))
+    val normalized = normalize(points.map(_.rotationAngle)) zip normalize(points.map(_.time.getTime.toDouble)) zip normalize(points.map(_.value))
     plotNormalized(normalized, g)
   }
 
   def galacticPlot(g: Graphics2D) {
-    val galacticCoords = filteredPoints.map(p => (p.standCoordinate, p.value)).map(c => (StarCoordsConverter.toGalacticCoordinates(FullStandCoordinate(c._1, LabCoordinates(37.686, 55.765, 135))), c._2))
+    val galacticCoords = points.map(p => (p.standCoordinate, p.value)).map(c => (StarCoordsConverter.toGalacticCoordinates(FullStandCoordinate(c._1, LabCoordinates(37.686, 55.765, 135))), c._2))
     val projected = galacticCoords.map(c => (MollweideProjection.project(c._1.l, c._1.b), c._2)).map(p => NormalizedColorPoint(NormalizedPoint(p._1._1, p._1._2), p._2))
     val projectedCmbr = cmbrPoints.map(MollweideProjection.project).map(p => NormalizedCMBR(NormalizedPoint(p._1, p._2)))
     val normalizedPointHolders = normalizePointHolders(projected ++ projectedCmbr)
@@ -71,12 +79,12 @@ class StarCoordsPlotter extends Component {
     values.map(_.normalize(minPoint, maxPoint))
   }
 
-  override protected def paintComponent(g: Graphics2D) {
+  def paint(g: Graphics2D) {
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY)
-    g.clearRect(0, 0, size.getWidth.toInt, size.getHeight.toInt)
+    g.clearRect(0, 0, width, height)
     val border = 10
-    g.scale((size.getWidth - border*2) / size.getWidth, (size.getHeight - border*2) / size.getHeight)
+    g.scale((width - border*2) * 1.0 / width, (height - border*2) * 1.0 / height)
     g.translate(border, border)
 
     viewMode match {
